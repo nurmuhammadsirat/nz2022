258cb8356c67a78e49568801939f1142f485778a
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2022-08-08 21:39:11 +0800

    Use base64 encoded private key.

--- netlify/functions/google-sheet-trip/google-sheet-trip.ts
+++ netlify/functions/google-sheet-trip/google-sheet-trip.ts
@@ -1,12 +1,13 @@
-import { Handler } from '@netlify/functions'
+import { Handler } from '@netlify/functions';
 import { google } from 'googleapis';
 
-
 const camelize = str => {
-  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-    return index === 0 ? word.toLowerCase() : word.toUpperCase();
-  }).replace(/\s+/g, '');
-}
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    })
+    .replace(/\s+/g, '');
+};
 
 export const handler: Handler = async (_event, _context) => {
   const spreadsheetId = '1s6PXdY6Qw4sEHBfTZH_xdXcolmzjnrbVAFhqozP6zmw';
@@ -16,40 +17,40 @@ export const handler: Handler = async (_event, _context) => {
   const jwt = new google.auth.JWT({
     email: process.env.GOOGLE_CLIENT_EMAIL!,
     scopes: 'https://www.googleapis.com/auth/spreadsheets',
-    key: process.env.GOOGLE_PRIVATE_KEY!,
+    key: atob(process.env.GOOGLE_PRIVATE_KEY!),
   });
 
   await jwt.authorize();
   google.options({ auth: jwt });
   const sheets = google.sheets('v4');
   const [accomodationRows, vehicleRows] = await Promise.all([
-    (await sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: accomodationRange,
-    })).data.values,
-    (await sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: vehicleRange,
-    })).data.values,
+    (
+      await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range: accomodationRange,
+      })
+    ).data.values,
+    (
+      await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range: vehicleRange,
+      })
+    ).data.values,
   ]);
 
-  const accomodationHeaders = accomodationRows!.shift()?.map(header => camelize(header))
+  const accomodationHeaders = accomodationRows!.shift()?.map(header => camelize(header));
   const vehicleHeaders = vehicleRows!.shift()?.map(header => camelize(header));
 
   const accomodations = accomodationRows!.map(row => {
-    return accomodationHeaders?.reduce((acc, elem, index) => (
-      { ...acc, [elem]: row[index]}
-    ), {})
+    return accomodationHeaders?.reduce((acc, elem, index) => ({ ...acc, [elem]: row[index] }), {});
   });
 
   const vehicles = vehicleRows!.map(row => {
-    return vehicleHeaders?.reduce((acc, elem, index) => (
-      { ...acc, [elem]: row[index]}
-    ), {})
+    return vehicleHeaders?.reduce((acc, elem, index) => ({ ...acc, [elem]: row[index] }), {});
   });
 
   return {
     statusCode: 200,
     body: JSON.stringify({ accomodations, vehicles }),
-  }
-}
+  };
+};
