fe930024f1252030c36aa302fabef728797bb5a0
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2022-08-08 23:53:42 +0800

    Abstract and clean up netlify function.

--- netlify/functions/google-sheet-trip/google-sheet-trip.ts
+++ netlify/functions/google-sheet-trip/google-sheet-trip.ts
@@ -9,16 +9,22 @@ const camelize = str => {
     .replace(/\s+/g, '');
 };
 
-export const handler: Handler = async (_event, _context) => {
-  const spreadsheetId = '1s6PXdY6Qw4sEHBfTZH_xdXcolmzjnrbVAFhqozP6zmw';
-  const accomodationRange = 'Accomodation!A1:L13';
-  const vehicleRange = 'Vehicle!A1:N4';
+const toObject = (row: string[]) => (acc, elem, index) => ({ ...acc, [elem]: row[index] });
 
-  const jwt = new google.auth.JWT({
-    email: process.env.GOOGLE_CLIENT_EMAIL!,
-    scopes: 'https://www.googleapis.com/auth/spreadsheets',
-    key: Buffer.from(process.env.ENCODED_GOOGLE_PRIVATE_KEY!, 'base64').toString('utf8').split('\\n').join('\n'),
-  });
+const sheetMetaData = {
+  spreadsheetId: '1s6PXdY6Qw4sEHBfTZH_xdXcolmzjnrbVAFhqozP6zmw',
+  accomodationRange: 'Accomodation!A1:L13',
+  vehicleRange: 'Vehicle!A1:N4',
+};
+
+const authProps = {
+  email: process.env.GOOGLE_CLIENT_EMAIL!,
+  scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  key: Buffer.from(process.env.ENCODED_GOOGLE_PRIVATE_KEY!, 'base64').toString('utf8').split('\\n').join('\n'),
+};
+
+export const handler: Handler = async (_event, _context) => {
+  const jwt = new google.auth.JWT(authProps);
 
   await jwt.authorize();
   google.options({ auth: jwt });
@@ -26,27 +32,27 @@ export const handler: Handler = async (_event, _context) => {
   const [accomodationRows, vehicleRows] = await Promise.all([
     (
       await sheets.spreadsheets.values.get({
-        spreadsheetId,
-        range: accomodationRange,
+        spreadsheetId: sheetMetaData.spreadsheetId,
+        range: sheetMetaData.accomodationRange,
       })
     ).data.values,
     (
       await sheets.spreadsheets.values.get({
-        spreadsheetId,
-        range: vehicleRange,
+        spreadsheetId: sheetMetaData.spreadsheetId,
+        range: sheetMetaData.vehicleRange,
       })
     ).data.values,
   ]);
 
-  const accomodationHeaders = accomodationRows!.shift()?.map(header => camelize(header));
-  const vehicleHeaders = vehicleRows!.shift()?.map(header => camelize(header));
+  const accomodationHeaders = accomodationRows?.shift()?.map(header => camelize(header));
+  const vehicleHeaders = vehicleRows?.shift()?.map(header => camelize(header));
 
-  const accomodations = accomodationRows!.map(row => {
-    return accomodationHeaders?.reduce((acc, elem, index) => ({ ...acc, [elem]: row[index] }), {});
+  const accomodations = accomodationRows?.map(row => {
+    return accomodationHeaders?.reduce(toObject(row), {});
   });
 
-  const vehicles = vehicleRows!.map(row => {
-    return vehicleHeaders?.reduce((acc, elem, index) => ({ ...acc, [elem]: row[index] }), {});
+  const vehicles = vehicleRows?.map(row => {
+    return vehicleHeaders?.reduce(toObject(row), {});
   });
 
   return {
