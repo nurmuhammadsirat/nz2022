5df69139d6bafac56caa10fc81aa06499118597e
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2022-08-11 18:04:01 +0800

    Added google maps link button only on check in and current accomodation.

--- src/pages/LandingPage/QuickInfoCard.tsx
+++ src/pages/LandingPage/QuickInfoCard.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Link, Text } from '@chakra-ui/react';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { Box, Flex, IconButton, Text } from '@chakra-ui/react';
+import { faAirbnb, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faCampground, faHotel, faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useCallback, useMemo } from 'react';
 import { Colors } from '../../styles';
@@ -11,60 +12,70 @@ type Props = {
   checkOutAccomodation?: Accomodation;
 };
 
-const ACCOMTYPE: { [key: string]: { name: string; color: string; time: string } } = {
+const ACCOMTYPE: { [key: string]: { name: string; color: string; time: string; showIcon: boolean } } = {
   checkout: {
-    name: 'Checking Out',
+    name: 'Checking Out @ 10:00 am',
     color: Colors.trafficLight.red,
     time: '10:00 AM',
+    showIcon: false,
   },
   checkin: {
-    name: 'Checking In',
+    name: 'Checking In @ 3:00 pm',
     color: Colors.trafficLight.green,
     time: '3:00 PM',
+    showIcon: true,
   },
   current: {
     name: 'Currently At',
     color: Colors.trafficLight.amber,
     time: '-',
+    showIcon: true,
   },
 };
 
+const iconType: { [key: string]: IconDefinition } = {
+  Airbnb: faAirbnb,
+  'Powered Site': faCampground,
+};
+
 const QuickInfoCard = ({ date, checkInAccomodation, checkOutAccomodation }: Props) => {
+  const handleLinkOpen = (url: string) => {
+    window.open(url, '_blank')!.focus();
+  };
+
   const isCheckingIn = useMemo(() => date === checkInAccomodation?.checkIn, [checkInAccomodation, date]);
 
   const accomodationInfo = useCallback(
     (acc: Accomodation, type: string) => (
-      <>
+      <Flex flexDirection="column" justifyContent="space-between">
         <Flex justifyContent="space-between">
-          <Box w="200px" maxH="75px">
+          <Box w="100%" maxH="75px">
             <Text as="i" fontSize="sm" fontWeight="bold" color={ACCOMTYPE[type].color}>
               {ACCOMTYPE[type].name}
             </Text>
-            <Text>{acc.name}</Text>
-          </Box>
-          {type !== 'current' && (
-            <Box w="70px">
-              <Text as="i" fontSize="sm" fontWeight="bold" color={ACCOMTYPE[type].color}>
-                Time
+            <Flex justifyContent="space-between">
+              <Text>
+                <FontAwesomeIcon icon={iconType[acc.type] || faHotel} /> {acc.name}
               </Text>
-              <Text>{ACCOMTYPE[type].time}</Text>
-            </Box>
-          )}
-        </Flex>
-        {type !== 'checkout' && (
-          <Box w="100%" textAlign="right" color={Colors.link} fontSize="xs">
-            <Link href={acc.googleMapsUrl} isExternal>
-              Google Maps <FontAwesomeIcon icon={faLocationDot} />
-            </Link>
+              {ACCOMTYPE[type].showIcon && (
+                <IconButton
+                  w="40px"
+                  h="40px"
+                  colorScheme="blue"
+                  aria-label="Google Maps"
+                  icon={<FontAwesomeIcon icon={faMapLocationDot} onClick={() => handleLinkOpen(acc.googleMapsUrl)} />}
+                />
+              )}
+            </Flex>
           </Box>
-        )}
-      </>
+        </Flex>
+      </Flex>
     ),
     [],
   );
 
   return (
-    <Flex flexDirection="column" gap="5px" pl="15px" minH="250px" w="calc(100% - 90px)">
+    <Flex flexDirection="column" gap="10px" pl="15px" minH="250px" w="calc(100% - 90px)">
       {checkOutAccomodation && accomodationInfo(checkOutAccomodation, 'checkout')}
       {checkInAccomodation && isCheckingIn && accomodationInfo(checkInAccomodation, 'checkin')}
       {checkInAccomodation && !isCheckingIn && accomodationInfo(checkInAccomodation, 'current')}
