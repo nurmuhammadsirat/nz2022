6c0ed7ff75c6006229c620d12ef92d437a788732
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2022-09-06 20:25:57 +0800

    Refactored and simplified data retrieval from Google Sheet.

--- netlify/functions/google-sheet-trip/google-sheet-trip.ts
+++ netlify/functions/google-sheet-trip/google-sheet-trip.ts
@@ -11,11 +11,14 @@ const camelize = str => {
 
 const toObject = (row: string[]) => (acc, elem, index) => ({ ...acc, [elem]: row[index] });
 
-const sheetMetaData = {
+const googleSheet = {
   spreadsheetId: '1s6PXdY6Qw4sEHBfTZH_xdXcolmzjnrbVAFhqozP6zmw',
-  accomodationRange: 'Accomodation!A1:L13',
-  vehicleRange: 'Vehicle!A1:N4',
-  flightRange: 'Flight!A1:K5',
+  ranges: [
+    ['Accomodation', 'Accomodation!A1:L13'],
+    ['Vehicle', 'Vehicle!A1:N4'],
+    ['Flight', 'Flight!A1:K5'],
+    ['Activity', 'Activity!A1:F5'],
+  ],
 };
 
 const authProps = {
@@ -30,46 +33,28 @@ export const handler: Handler = async (_event, _context) => {
   await jwt.authorize();
   google.options({ auth: jwt });
   const sheets = google.sheets('v4');
-  const [accomodationRows, vehicleRows, flightRows] = await Promise.all([
-    (
-      await sheets.spreadsheets.values.get({
-        spreadsheetId: sheetMetaData.spreadsheetId,
-        range: sheetMetaData.accomodationRange,
-      })
-    ).data.values,
-    (
-      await sheets.spreadsheets.values.get({
-        spreadsheetId: sheetMetaData.spreadsheetId,
-        range: sheetMetaData.vehicleRange,
-      })
-    ).data.values,
-    (
-      await sheets.spreadsheets.values.get({
-        spreadsheetId: sheetMetaData.spreadsheetId,
-        range: sheetMetaData.flightRange,
-      })
-    ).data.values,
-  ]);
+  const NZTripData = await Promise.all(
+    googleSheet.ranges.map(
+      async ([_sheetName, sheetRange]) =>
+        (
+          await sheets.spreadsheets.values.get({
+            spreadsheetId: googleSheet.spreadsheetId,
+            range: sheetRange,
+          })
+        ).data.values,
+    ),
+  );
 
-  const accomodationHeaders = accomodationRows?.shift()?.map(header => camelize(header));
-  const vehicleHeaders = vehicleRows?.shift()?.map(header => camelize(header));
-  const flightHeaders = flightRows?.shift()?.map(header => camelize(header));
-
-  const accomodations = accomodationRows?.map(row => {
-    return accomodationHeaders?.reduce(toObject(row), {});
-  });
-
-  const vehicles = vehicleRows?.map(row => {
-    return vehicleHeaders?.reduce(toObject(row), {});
-  });
-
-  const flights = flightRows?.map(row => {
-    return flightHeaders?.reduce(toObject(row), {});
+  const [accomodations, vehicles, flights, activities] = NZTripData.map(rows => {
+    const headers = rows?.shift()?.map(header => camelize(header));
+    return rows?.map(row => {
+      return headers?.reduce(toObject(row), {});
+    });
   });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ accomodations, vehicles, flights }),
+    body: JSON.stringify({ accomodations, vehicles, flights, activities }),
     headers: {
       'Content-Type': 'application/json',
     },
