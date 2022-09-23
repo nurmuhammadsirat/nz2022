0627e40ce269d4a9a60dddd1febfff5e89aaaee9
Nur Muhammad Bin Sirat <mat.ace@gmail.com>
2022-09-23 17:59:45 +0800

    Hide travel date on scroll.

--- src/components/Main/Header.tsx
+++ src/components/Main/Header.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Center, Collapse, Flex } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import backgroundImage from '../../assets/negative-space-truck-road-mountains.jpg';
 import { Colors } from '../../styles';
 
@@ -7,25 +7,52 @@ type Props = {
   height: number;
 };
 
+const innerHeight = 70;
+
 const Header = ({ height }: Props) => {
+  const [showDate, setShowDate] = useState(true);
+
+  useEffect(() => {
+    const print = () => {
+      if (window.scrollY > 30) {
+        setShowDate(false);
+      } else {
+        setShowDate(true);
+      }
+    };
+
+    window.addEventListener('scroll', print);
+
+    return () => {
+      window.removeEventListener('scroll', print);
+    };
+  }, []);
+
   return (
-    <Box
+    <Flex
       h={`${height}px`}
       flexDirection="column"
       justifyContent="center"
       backgroundImage={backgroundImage}
       backgroundPosition="center"
       backgroundSize="cover"
+      position="sticky"
+      top={`${innerHeight - height}px`}
+      zIndex={2}
     >
       <Flex w="100%" h={height} flexDirection="column" justifyContent="center" backgroundColor="rgb(0, 0, 0, 0.4)">
-        <Text align="center" fontSize="40px" color={Colors.headerText}>
-          New Zealand 2022
-        </Text>
-        <Text align="center" fontSize="20px" color={Colors.headerText}>
-          Nov 30 - Dec 26
-        </Text>
+        <Box h={`${innerHeight}px`} position="sticky" top="0">
+          <Center fontSize="40px" color={Colors.headerText}>
+            New Zealand 2022
+          </Center>
+          <Collapse in={showDate}>
+            <Center fontSize="20px" color={Colors.headerText}>
+              Nov 30 - Dec 26
+            </Center>
+          </Collapse>
+        </Box>
       </Flex>
-    </Box>
+    </Flex>
   );
 };
 
